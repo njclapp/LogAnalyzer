c8c0a504d795922570b3f8648fcd109f048b6a58
nate <nate@localhost.localdomain>
2017-01-25 22:16:32 -0600

    Added open/read file

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
 #Log Analyzer for UFW.log files
+import re
 
-import os
+#define lists
+date = []
+ip = []
+port = []
+#read ufw.log to memory
+f =	open('ufw.txt', 'r')
+
+#loop through file for Date/Time, source IP(SRC),  and port scanned(DPT)
+for i in f:
+	f.readline()
+#write output file(ufwLog+currdate)
+
+#close ufw.log
+f.close()
