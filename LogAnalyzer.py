e054e9985fc60c6bd0cd0e9273824cf6f08c3e37
root <root@localhost.localdomain>
2017-01-30 20:47:42 -0600

    Regex bullshit partially done

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -2,17 +2,14 @@
 #Log Analyzer for UFW.log files
 import re
 
-#define lists
-date = []
-ip = []
-port = []
-#read ufw.log to memory
-f =	open('ufw.txt', 'r')
-
 #loop through file for Date/Time, source IP(SRC),  and port scanned(DPT)
-for i in f:
-	f.readline()
+with open('ufw.txt', 'r') as f: #opens file as f and is used to loop through
+	line = f.read()
+	date = re.findall('\w\w\w\s\d\d\s\d\d:\d\d:\d\d', line, flags = 0)
+	print(date)
+	IP = re.findall('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line, flags = 0)
+	print(IP)
 #write output file(ufwLog+currdate)
 
-#close ufw.log
+#elose ufw.log
 f.close()
