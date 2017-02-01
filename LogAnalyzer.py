c861bac019af7c862f99598c946686aede7da39e
njclapp <njclapp@pluto.dsu.edu>
2017-01-31 18:44:59 -0600

    Added DPT functionality and write to file 'output.txt'

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -6,12 +6,21 @@ import re
 with open('ufw.txt', 'r') as f: #opens file as f and is used to loop through
 	line = f.read()
 	date = re.findall('\w\w\w\s\d\d\s\d\d:\d\d:\d\d', line, flags = 0)
-	#print(date)
 	IP = re.findall('\d[^\192]{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line, flags = 0)
-	#print(IP)
 	DPT = re.findall('DPT=(\d{1,5})', line, flags = 0)
-	print DPT
-#write output file(ufwLog+currdate)
 
 #close ufw.log
+f.close()
+
+#Write output file
+f = open('output.txt', 'w')
+i = 0
+while date != '':
+	try:
+		f.write(date[i]+'\t'+IP[i]+'\t'+DPT[i]+'\n\n')
+		i+=1
+	except IndexError:
+		print("Done.")
+		break
+
 f.close()
