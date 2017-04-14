3001c680937347e137d32314cf128e908b9c3fb9
njclapp <njclapp@pluto.dsu.edu>
2017-04-14 15:46:08 -0500

    Add stats for protocols in output

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -10,9 +10,19 @@ subprocess.call(['sh', 'moveUFW.sh'])
 #loop through file for Date/Time, source IP(SRC),  and port scanned(DPT)
 with open('ufw.txt', 'r') as f: #opens file as f and is used to loop through file
 	line = f.read()
+
+	ftp = re.findall('DPT=21', line, flags = 0)
+	ssh = re.findall('DPT=22', line, flags = 0)
+	telnet = re.findall('DPT=23', line, flags = 0)
+	dns = re.findall('DPT=53', line, flags = 0)
+	http = re.findall('DPT=80', line, flags = 0)
+	ntp = re.findall('DPT=123', line, flags = 0)
+	l33t = re.findall('DPT=1337', line, flags = 0)
+
 	date = re.findall('\w\w\w\s{1,}\d{1,}\s\d\d:\d\d:\d\d', line, flags = 0)
 	IP = re.findall('SRC=(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})', line, flags = 0)
 	DPT = re.findall('DPT=(\d{1,5})', line, flags = 0)
+
 
 #Write output file
 yesterday = datetime.datetime.now() - datetime.timedelta(days=1) #since the cron is set for midnight, we have to subtract one day to make the log fit the date
@@ -21,6 +31,14 @@ formatting = "{0:15s}		{1:15}		{2:5}" #spaces out date, IP, and DPT to make the 
 
 f = open(filestring, 'w')
 i=0
+f.write("FTP: "+ str(len(ftp))+'\n')
+f.write("SSH: "+ str(len(ssh))+'\n')
+f.write("Telnet: "+ str(len(telnet))+'\n')
+f.write("DNS: "+ str(len(dns))+'\n')
+f.write("HTTP: "+ str(len(http))+'\n')
+f.write("NTP: "+ str(len(ntp))+'\n')
+f.write("l33t: "+ str(len(l33t))+'\n')
+f.write("Total: "+str(len(date))+'\n\n')
 
 while date != '': #note: when i hits '', it causes an IndexError which is handled by the except
 	try:
