5d0ba1396a5bcd43f32428ccc3b162ba510b0ec6
Nate Clapp <njclapp@pluto.dsu.edu>
2018-04-11 22:27:41 -0500

    Refactor code for class project

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3.5
-#Log Analyzer for UFW.log files
+# Log Analyzer for UFW.log files
+# Contributors: Nate Clapp <njclapp@pluto.dsu.edu>
+
 import re
 import datetime
 import subprocess
+import os
 
-#grep ufw.log for blocked IP addresses
-w = open("ufw.txt","w")
-subprocess.call(['grep BLOCK /var/log/ufw.log'], stdout=w, shell=True)
-subprocess.call(['>', '/var/log/ufw.log'], shell=True)
-subprocess.call('mv output* reports/', shell=True)
+# Attempt to move any previous output files to reports directory
+try:
+	os.stat('reports/')
+	subprocess.call('mv output* reports/', shell=True)
+except OSError:
+	os.mkdir('reports')
 
-#loop through file for Date/Time, source IP(SRC),  and port scanned(DPT)
+# Essentially greps any line that says BLOCK and places it in ufw.txt
+with open('ufw.txt', 'w') as w:
+	for line in open("/var/log/ufw.log","r"):
+		if 'BLOCK' in line:
+			w.write(line)
+
+# To clear ufw.log each night
+open('/var/log/ufw.log','w').close() 
+
+#loop through file for Date/Time, source IP(SRC), and port scanned(DPT)
 with open('ufw.txt', 'r') as f: #opens file as f and is used to loop through file
 	line = f.read()
 
@@ -33,24 +46,23 @@ yesterday = datetime.datetime.now() - datetime.timedelta(days=1) #since the cron
 filestring = 'output'+str(yesterday.strftime("%Y%m%d"))+'.txt' #automatically includes current date in filename
 formatting = "{0:15s}		{1:15}		{2:5}" #spaces out date, IP, and DPT to make the output look nice
 
-f = open(filestring, 'w')
-i=0
+with open(filestring, 'w') as f:
+	i=0
 
-f.write("FTP: "+ str(len(ftp)) + ' (' + str(float((len(ftp))/float(len(date))*100))[0:4] + '%)\n')
-f.write("SSH: "+ str(len(ssh)) + ' (' + str(float((len(ssh))/float(len(date))*100))[0:4] + '%)\n')
-f.write("Telnet: "+ str(len(telnet)) + ' (' + str(float((len(telnet))/float(len(date))*100))[0:4] + '%)\n')
-f.write("DNS: "+ str(len(dns)) + ' (' + str(float((len(dns))/float(len(date))*100))[0:4] + '%)\n')
-f.write("HTTP: "+ str(len(http)) + ' (' + str(float((len(http))/float(len(date))*100))[0:4] + '%)\n')
-f.write("NTP: "+ str(len(ntp)) + ' (' + str(float((len(ntp))/float(len(date))*100))[0:4] + '%)\n')
-f.write("l33t: "+ str(len(l33t)) + ' (' + str(float((len(l33t))/float(len(date))*100))[0:4] + '%)\n')
-f.write("Other: "+ str(other) + ' (' + str(float(other)/float(len(date))*100)[0:4] + '%)\n')
-f.write("Total: "+str(len(date)) + '\n\n')
+	f.write("FTP: "+ str(len(ftp)) + ' (' + str(float((len(ftp))/float(len(date))*100))[0:4] + '%)\n')
+	f.write("SSH: "+ str(len(ssh)) + ' (' + str(float((len(ssh))/float(len(date))*100))[0:4] + '%)\n')
+	f.write("Telnet: "+ str(len(telnet)) + ' (' + str(float((len(telnet))/float(len(date))*100))[0:4] + '%)\n')
+	f.write("DNS: "+ str(len(dns)) + ' (' + str(float((len(dns))/float(len(date))*100))[0:4] + '%)\n')
+	f.write("HTTP: "+ str(len(http)) + ' (' + str(float((len(http))/float(len(date))*100))[0:4] + '%)\n')
+	f.write("NTP: "+ str(len(ntp)) + ' (' + str(float((len(ntp))/float(len(date))*100))[0:4] + '%)\n')
+	f.write("l33t: "+ str(len(l33t)) + ' (' + str(float((len(l33t))/float(len(date))*100))[0:4] + '%)\n')
+	f.write("Other: "+ str(other) + ' (' + str(float(other)/float(len(date))*100)[0:4] + '%)\n')
+	f.write("Total: "+str(len(date)) + '\n\n')
 
-while date != '': #note: when i hits '', it causes an IndexError which is handled by the except
-	try:
-		f.write(formatting.format(date[i], IP[i], DPT[i])+'\n')
-		i+=1
-	except IndexError: #used to overwrite error message that appears when 'i' hits the end of the list and tries to find an index that does not exist
-		print("Done.")
-		break
-f.close()
+	while date != '': #note: when i hits '', it causes an IndexError which is handled by the except	
+		try:
+			f.write(formatting.format(date[i], IP[i], DPT[i])+'\n')
+			i+=1
+		except IndexError: #used to overwrite error message that appears when 'i' hits the end of the list and tries to find an index that does not exist
+			print("Done.")
+			break
